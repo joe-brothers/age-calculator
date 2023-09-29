c2e18ba27f947424027438b27f5f9a5c2c5f06d6
Ahra Cho <choar816@postech.ac.kr>
2023-09-29 19:07:16 +0900

    Add ad

--- src/main.tsx
+++ src/main.tsx
@@ -9,5 +9,10 @@ window.Kakao.isInitialized();
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <script
+      async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4125713536443110"
+      crossOrigin="anonymous"
+    ></script>
   </React.StrictMode>
 );
